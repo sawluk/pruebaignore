3ad5f4eac9d591bd6279739a732761e12e1994d3
sawluk <sawlukbloos@gmail.com>
2025-10-02 11:49:56 -0500

    reducir comentarios

--- ej1.py
+++ ej1.py
@@ -9,4 +9,4 @@ def comparar_cadenas(cadena1, cadena2):
 cadena_a = "Hola"
 cadena_b = "Hola"
 resultado = comparar_cadenas(cadena_a, cadena_b)
-print(resultado)  # Salida: Las cadenas son iguales
+print(resultado)  
