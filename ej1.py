9ecfb795bfa82629db06c14872cbafca1e312685
sawluk <sawlukbloos@gmail.com>
2025-10-19 14:52:48 -0500

    Mejorar funcion para comparar cadenas sin distinguir mayusculas

--- ej1.py
+++ ej1.py
@@ -1,12 +1,12 @@
-# funcion para comparar cadenas de texto
+# funcion para comparar cadenas de texto, ignorando mayúsculas/minúsculas
 def comparar_cadenas(cadena1, cadena2):
-    if cadena1 == cadena2:
+    if cadena1.lower() == cadena2.lower():
         return "Las cadenas son iguales"
     else:
         return "Las cadenas son diferentes"
 
 # ejemplo de uso
 cadena_a = "Hola"
-cadena_b = "Hola"
+cadena_b = "hola"
 resultado = comparar_cadenas(cadena_a, cadena_b)
-print(resultado)  
+print(resultado)
